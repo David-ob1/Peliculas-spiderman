e8b928c5c43c7384a3c84fbcfb7f4c1c53466d09
David-ob1 <davidvazquezt2001@gmail.com>
2024-06-28 16:47:05 -0300

    creacion de funciones

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 const api = "./api.json"
 
+const $containerMovies = document.getElementById("container")
+
 fetch(api)
  .then(response => response.json())
  .then(data => {
     const arrayMovies = data
 
-    arrayMovies.map(element => {
+   let templateMovies = arrayMovies.map(element => {
        let title = element.Title
        let image = element.Poster
        console.log(title)
@@ -19,12 +21,78 @@ fetch(api)
             <h2>${title}</h2>
        </div>
        `
-        document.getElementById("container").innerHTML += poster
+       return poster
+       
 
 
     })
+
+    let joinedTemplateMovies = templateMovies.join('')
+    displayElement($containerMovies,joinedTemplateMovies)
+
+    let checkbox = SetXpropiedad(arrayMovies,"Distributors")
+
+    
+    
 
  })
 
 
  .catch(error => console.log(error))
+
+// capturas elementos del HTML
+ const buscador = document.getElementById("buscador")
+ const contenedorCheck = document.getElementById("checkbox-container")
+
+
+
+
+
+//  CREACION E IMPRESION DE ELEMENTOS DINAMICOS
+function SetXpropiedad (array,property){
+    arrayOfproperty = array.map( item => item[property])
+   return Array.from( new Set (arrayOfproperty))
+}
+
+
+function checkboxTemplate (pelicula){
+
+   return ` <input type="checkbox" name="" id="${pelicula}">
+            <label for="checkbox">${pelicula}</label>
+            `
+}
+
+
+
+
+//  FILTROS DE USUARIO
+function filtroXnombre(array,text,property){
+   let  value = text.toLoweCase()
+   let filter = array.filter( movie => movie[property].toLoweCase().includes(value))
+  
+   return filter
+}
+
+function filtroCheck(array,checkSelected,property){
+
+   if(checkSelected.length == 0){
+      return array
+   }
+
+  let filter = array.filter(item => checkSelected.includes(item[property]))
+
+   // checkSelected
+
+}
+
+
+   //  buscador.addEventListener("change",()=>{
+   //    console.log(buscador.value)
+   //  })
+
+
+function displayElement(container,template){
+
+   container.innerHTML = template
+
+}
